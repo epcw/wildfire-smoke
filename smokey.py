cc72d7b160d02290aa4a903dc759419f67980363
Patrick W. Zimmerman <pkwzimm@gmail.com>
2022-12-15 12:48:29 -0800

    More plots (AQI, AQI100, AQI150, AQIlog)

--- smokey.py
+++ smokey.py
@@ -7,17 +7,47 @@ df = pd.read_csv('data/aqi_data_stacked.csv')
 print('loading dataframe')
 df['Observation Time'] = pd.to_datetime(df['Observation Time'])
 
-fig, ax = plt.subplots(figsize=(16,10))
-print('drawing graph')
+fig, ax = plt.subplots(figsize=(30,16))
+
+ax.set(xlabel='Time', ylabel='AQI')
+print('drawing graph: AQI')
 sns.lineplot(x='Observation Time', y='value', hue='measure',data=df)
 
-plt.title('AQI')
+plt.title('Puget Sound Air Quality Index over time')
 
-# x = df['Observation Time']
-# y = df['value']
-
-# for index in range(len(x)):
-#     ax.text(x[index],y[index], size=7)
-
-# plt.show()
 plt.savefig('AQI.png')
+
+fig, ax = plt.subplots(figsize=(30,16))
+
+ax.set_yscale('log')
+ax.set(xlabel='Time', ylabel='AQI')
+print('drawing graph: AQI (log scale)')
+sns.lineplot(x='Observation Time', y='value', hue='measure',data=df)
+
+plt.title('Puget Sound Air Quality Index over time (log scale)')
+
+plt.savefig('AQI_log.png')
+
+df150 = df[(df['value'] >= 150)] #select for days where schools will close
+
+fig, ax = plt.subplots(figsize=(30,16))
+
+ax.set(xlabel='Time', ylabel='AQI')
+print('drawing graph: AQI 150+')
+sns.scatterplot(x='Observation Time', y='value', hue='measure',data=df150, s=10)
+
+plt.title('Puget Sound Air Quality Index: days 150+')
+
+plt.savefig('AQI_150.png')
+
+df100 = df[(df['value'] >= 100)] #select for days with unhealthy air
+
+fig, ax = plt.subplots(figsize=(30,16))
+
+ax.set(xlabel='Time', ylabel='AQI')
+print('drawing graph: AQI 100+')
+sns.scatterplot(x='Observation Time', y='value', hue='measure',data=df100, s=100)
+
+plt.title('Puget Sound Air Quality Index: days 100+')
+
+plt.savefig('AQI_100.png')
