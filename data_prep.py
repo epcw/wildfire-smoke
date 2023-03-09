48de358617510cd5b6619c42e85b15a35c97e8a2
Patrick W. Zimmeman <pzimmerman@epcw.org>
2023-03-09 10:17:25 -0800

    Have data displaying over map for 1/28/2023, with compressed colorscale for testing visibility.  Currently using a simple blue rect as background. TODO: get a real basemap

--- data_prep.py
+++ data_prep.py
@@ -11,6 +11,7 @@ import geoplot as gplt
 import matplotlib.pyplot as plt
 import matplotlib
 import cartopy.crs as ccrs
+import csv
 
 filename = 'data/pa_hist_data_avg.parquet'
 station_filename = 'data/pa_station_list.csv'
@@ -21,10 +22,21 @@ parqf = pf(filename)
 df = parqf.to_pandas()
 df_stations = pd.read_csv(station_filename)
 
-wa_shp = gpd.read_file(geo_filename)
-wa_select_counties = wa_shp[wa_shp['COUNTYFP'].isin(['009','029','031','033','035','045','053','055','057','061','067','073'])] # select only counties within Puget Sound south of Pt Townsend
+df_stations_temp = df_stations[['sensor_index','name','latitude','longitude','altitude']]
+df_stations_temp = df_stations_temp.rename(columns={"sensor_index": "station_index"})
+df_temp = df[['station_index','time_stamp','pm2.5_AVG']]
+df_temp = df_temp.rename(columns={"pm2.5_AVG": "pm2_5_AVG"}) # this is so D3 doesn't go nuts on the decimal point
+df_temp = df_temp[(df_temp['time_stamp'] > 1672592400) & (df_temp['time_stamp'] < 1672642800)].drop_duplicates()
 
-df_stations['geometry'] = gpd.points_from_xy(df_stations['longitude'], df_stations['latitude'])
+# spit out merged datafile for d3, which does not like parquet
+d3_df = df_temp.merge(df_stations_temp, left_on='station_index', right_on='station_index')
+d3_filename = 'map/stations.csv'
+d3_df.to_csv(d3_filename, index=False, quotechar='"', quoting=csv.QUOTE_ALL)
+
+# wa_shp = gpd.read_file(geo_filename)
+# wa_select_counties = wa_shp[wa_shp['COUNTYFP'].isin(['009','029','031','033','035','045','053','055','057','061','067','073'])] # select only counties within Puget Sound south of Pt Townsend
+#
+# df_stations['geometry'] = gpd.points_from_xy(df_stations['longitude'], df_stations['latitude'])
 #
 # ax = gplt.polyplot(
 #     wa_select_counties, projection=gcrs.AlbersEqualArea(),
@@ -48,40 +60,40 @@ min_lon = -123
 min_lat = 47
 max_lon = -122
 
-plot_col = 'pm2.5_24hour'
-plot_data = df_stations[df_stations[plot_col].notnull()]
-
-norm = matplotlib.colors.Normalize(vmin=0, vmax=np.percentile(plot_data[plot_col],97))
-
-ax = wa_select_counties.plot(
-    figsize=(20,30),
-    facecolor='#f6f0e8',
-    edgecolor='black'
-)
-
-gplt.pointplot(
-    gpd.GeoDataFrame(plot_data),
-    s=10,
-    hue='pm2.5_24hour',
-    cmap='RdYlGn_r',
-    ax=ax,
-    legend=True,
-    norm=norm
-)
-
-ccrs.PlateCarree()
-
-seattle_contours = gpd.read_file('data/shape/Elev_Contour.shp')
-
-with open('map/seattle_contours.geojson', 'w') as file:
-    file.write(seattle_contours.to_json())
-
-seattle_contours.plot(ax=ax, alpha=0.1)
-
-plt.title('24_hour AQI (as of Jan 2023)')
-
-ax.set_xlim((min_lon,max_lon))
-ax.set_ylim((min_lat,max_lat))
-
-plt.savefig('AQI_map_contour.png')
-plt.show()
+# plot_col = 'pm2.5_24hour'
+# plot_data = df_stations[df_stations[plot_col].notnull()]
+#
+# norm = matplotlib.colors.Normalize(vmin=0, vmax=np.percentile(plot_data[plot_col],97))
+#
+# ax = wa_select_counties.plot(
+#     figsize=(20,30),
+#     facecolor='#f6f0e8',
+#     edgecolor='black'
+# )
+#
+# gplt.pointplot(
+#     gpd.GeoDataFrame(plot_data),
+#     s=10,
+#     hue='pm2.5_24hour',
+#     cmap='RdYlGn_r',
+#     ax=ax,
+#     legend=True,
+#     norm=norm
+# )
+#
+# ccrs.PlateCarree()
+#
+# seattle_contours = gpd.read_file('data/shape/Elev_Contour.shp')
+#
+# # with open('map/seattle_contours.geojson', 'w') as file:
+# #     file.write(seattle_contours.to_json()) # use mapshaper.org instead - this is easier and you can simplify it rather than mess wit the CLI
+#
+# seattle_contours.plot(ax=ax, alpha=0.1)
+#
+# plt.title('24_hour AQI (as of Jan 2023)')
+#
+# ax.set_xlim((min_lon,max_lon))
+# ax.set_ylim((min_lat,max_lat))
+#
+# plt.savefig('AQI_map_contour.png')
+# plt.show()
--- map/aqi.js
+++ map/aqi.js
@@ -4,7 +4,7 @@ var margin = {top: 20, right: 30, bottom: 30, left: 40},
     h = 1101;
 
 // set-up unit projection and path
-var projection = d3.geo.mercator()
+const projection = d3.geo.mercator()
     .scale(1)
     .translate([0, 0]);
 
@@ -16,9 +16,17 @@ var svg = d3.select("#mapdiv")
   .append("svg")
     .attr("id","map")
     .attr("width", w + margin.left + margin.right)
-    .attr("height", h + margin.top + margin.bottom)
-  .append("g")
+    .attr("height", h + margin.top + margin.bottom);
+
+svg.append("g")
     .attr("id", "contour_map")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")")
+    .append("rect")
+    .attr("id","basemap");
+
+svg.append("g")
+    .attr("id","stations")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
@@ -31,10 +39,74 @@ const min_lon = -123
 const min_lat = 47
 const max_lon = -122
 
-d3.json("/aqi/map/seattle_contours_simplified.json", function(error, puget_sound) {
+var time_stamp = 1672617600
+
+
+// load the station data
+function stations() {
+d3.csv("/aqi/map/stations.csv", function(data) {
+
+//    var x = d3.scaleLinear()
+//    .domain([min_lon, max_lon])
+//    .range([0, width]);
+//
+//    var y = d3.scaleLinear()
+//    .domain([min_lat,max_lat])
+//    .range([height,0]);
+//    var mapwidth = d3.select("#contour_map").node().getBBox().width;
+//    var mapheight = d3.select("#contour_map").node().getBBox().height;
+//
+//    var colors = d3.scale.linear()
+//    .domain([0,50,150])
+//    .range(["green","yellow","red"])
+
+
+    svg.select("g#stations")
+    .append("g")
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) { var coords = projection([d.longitude, d.latitude]); return coords[0];})
+    .attr("cy", function(d) { var coords = projection([d.longitude, d.latitude]); return coords[1];})
+    .attr("r", 5)
+    .style("fill", function(d) { var aqi = d.pm2_5_AVG; return colorize(aqi)})
+    .style("stroke", "#2e2e2e")
+    .style("stroke-width", "1")
+
+});
+
+}
+function colorize (aqi) {
+
+    if (aqi < 5) {
+        color = "green"
+        }
+    else if (aqi >= 5 && aqi < 10) {
+        color = "yellow"
+        }
+    else if (aqi >= 10 && aqi < 15) {
+        color = "orange"
+        }
+    else if (aqi >= 15 && aqi < 20) {
+        color = "red"
+        }
+    else if (aqi >= 20 && aqi < 30) {
+        color = "purple"
+        }
+    else if (aqi >= 30 && aqi < 50) {
+        color = "#800000"
+        }
+    else {
+        color = "#000000"
+        }
+    return color
+}
+
+d3.json("/aqi/map/filtered_seattle_contours.json", function(error, puget_sound) {
   if (error) return console.error(error);
 
-  var bTopo = topojson.feature(puget_sound, puget_sound.objects.seattle_contours),
+  var bTopo = topojson.feature(puget_sound, puget_sound.objects.filtered_seattle_contours),
         topo = bTopo.features;
 
     var hRange = d3.extent(topo, function(d, i) {
@@ -52,7 +124,7 @@ d3.json("/aqi/map/seattle_contours_simplified.json", function(error, puget_sound
     projection.scale(s)
         .translate(t);
 
-    svg.selectAll("path")
+    svg.select("g#contour_map").selectAll(".path")
         .data(topo).enter()
         .append("path")
         .style("fill", "none")
@@ -64,10 +136,24 @@ d3.json("/aqi/map/seattle_contours_simplified.json", function(error, puget_sound
         .on("mouseout", function (d,i) {unhighlight(this,d);
         });
 
+    var mapwidth = d3.select("#contour_map").node().getBBox().width;
+    var mapheight = d3.select("#contour_map").node().getBBox().height;
+
+    svg.select("#basemap")
+        .attr("width", mapwidth)
+        .attr("height", mapheight)
+        .attr("transform",
+          "translate(140,28)")
+        .attr("fill","#D6EAFF");
+
+        stations();
 });
 
-// function to interpolate between to colours
+//d3.timeout(stations, 10000);
+
+// function to interpolate between two colours
 // see http://stackoverflow.com/questions/12217121/continuous-color-scale-from-discrete-domain-of-strings
+
 
 function interp(x) {
     var ans = d3.interpolateLab("#ffffe5", "#004529")(x);
@@ -78,7 +164,7 @@ function highlight(x) {
 
     var s = d3.select(this);
 
-    s.style("stroke", "red");
+    s.style("stroke", "#660066");
 
 }
 function unhighlight(x,y) {
